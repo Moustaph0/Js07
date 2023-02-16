01f38f2a02d8a9b4645691b1388e4ae36fe2fa13
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-16 13:04:53 +0100

    Update celsiusToFahrenheit.js

--- celsiusToFahrenheit.js
+++ celsiusToFahrenheit.js
@@ -3,8 +3,10 @@
 function celsiusToFahrenheit(celsius) 
 {
   let cTemp = celsius;
-  let cToFahr = (cTemp * 1.8) + 32;
-    console.log(cToFahr);
+  let Fahrenheit = (cTemp * 1.8) + 32;
+    console.log(Fahrenheit);
 }
 
 celsiusToFahrenheit(30);
+
+module.exports = celsiusToFahrenheit;
