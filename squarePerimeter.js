dc04cc60d4bacb66a2bcea1c47cd1e4bd65ccf48
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-16 18:37:41 +0100

    Update squarePerimeter.js

--- squarePerimeter.js
+++ squarePerimeter.js
@@ -1,11 +1,11 @@
 
 function squarePerimeter(longueur) {
-    let squareF = { perimeter: longueur*4, aria: longueur*longueur}
+    let squareF = { perimeter: longueur*4, area: longueur*longueur}
     
    return squareF;
 }
 let square = squarePerimeter(4);
 console.log("perimeter :", square.perimeter);
-console.log("area :", square.aria);
+console.log("area :", square.area);
 
 module.exports = squarePerimeter;
