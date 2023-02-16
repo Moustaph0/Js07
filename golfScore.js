090a11878159770bea05673661b5b7edc9f87345
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-16 13:38:43 +0100

    Update golfScore.js

--- golfScore.js
+++ golfScore.js
@@ -1,27 +1,29 @@
-function golfScore(par, coup) {
+function golfScore(par, strokes) {
     let score_names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bongey","Go Home!"];
-    if(coup == 1) { // this is always a hole in 1
+    if(strokes == 1) { // this is always a hole in 1
       return score_names[0]; // hole in one
-    }
-    if(coup === par) {
-      return score_names[3]; // on par
-    }
-    if(coup <= par - 2) {
-      return score_names[1]; // eagle
-    }
-    if(coup == par - 1) {
-      return score_names[2]; // birdie
-    }
-    if(coup >= par + 3) {
-      return score_names[6]; // go home
-    }
-    if(coup >= par + 2) {
-      return score_names[5]; // double bogie
-    }
-    if(coup >= par + 1) {
-      return score_names[4]; // bogie
-    }
+   }
+  if(strokes === par) {
+    return score_names[3]; // on par
+  }
+  if(strokes <= par - 2) {
+    return score_names[1]; // eagle
+  }
+  if(strokes == par - 1) {
+    return score_names[2]; // birdie
+  }
+  if(strokes >= par + 3) {
+    return score_names[6]; // go home
+  }
+  if(strokes >= par + 2) {
+    return score_names[5]; // double bogie
+  }
+  if(strokes >= par + 1) {
+    return score_names[4]; // bogie
+  }
     
   }
   
   console.log(golfScore(4,1));
+
+module.exports = console.log(golfScore);
