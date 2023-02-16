e64dce62c8ed6056dfbbaec80a328926889c392c
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-16 13:27:22 +0100

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,9 +1,12 @@
-function factorial(nbr){
+function factorial(n){
     let i, f = 1;
-    for(i = 1; i <= nbr; i++)  
+    for(i = 1; i <= n; i++)  
     {
       f *= i;   
     }  
     console.log(f);
   }
+
   factorial(5);
+
+module.exports = factorial;
